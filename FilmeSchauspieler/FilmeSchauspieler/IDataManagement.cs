e7f73ba299833a2f903fa07d8a53451ec09570b0
florian minnecker <florian@minnecker.com>
2018-10-09 12:35:53 +0200

    Interfaces und Klassen erstellt

---
+++ FilmeSchauspieler/FilmeSchauspieler/IDataManagement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FilmeSchauspieler.Classes;
+
+namespace FilmeSchauspieler
+{
+    interface IDataManagement
+    {
+        int countActors();
+        int countMovies();
+        List<Actor> getActors();
+        List<Movie> getMovies();
+        Actor getActor(int id);
+        Movie getMovie(int id);
+        bool addActor(Actor actor);
+        bool addMovie(Movie movie);
+        bool alterActor(Actor actor);
+        bool alterMovie(Movie movie);
+        bool deleteActor(Actor actor);
+        bool deleteMovie(Movie movie);
+
+    }
+}
---
+++ FilmeSchauspieler/FilmeSchauspieler/ISystemSpecification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FilmeSchauspieler.Classes;
+
+namespace FilmeSchauspieler
+{
+    interface ISystemSpecification
+    {
+        List<Movie> getMovies();
+        List<Actor> getActors();
+        Actor getActor(int id);
+        Movie getMovie(int id);
+        bool createActor(string name);
+        bool createMovie(string name);
+        bool alterActor(Actor actor);
+        bool alterMovie(Movie movie);
+        bool deleteActor(Actor actor);
+        bool deleteMovie(Movie movie);
+        List<Actor> getActors(Movie movie);
+        List<Movie> getMovies(Actor actor);
+        bool addActorToMovie(Actor actor, Movie movie);
+        bool removeActorFromMovie(Actor actor, Movie movie);
+
+    }
+}
